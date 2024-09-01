d46c3d2bd5bc071e17419eac5eb7e6926c1f2cc0
Ellio <sereneheart888@gmail.com>
2024-09-01 16:51:58 +0300

    [U] responsive for px'

--- components/RefreshBtn.tsx
+++ components/RefreshBtn.tsx
@@ -4,7 +4,7 @@ type Props = PropsWithChildren<{
 }>;
 export default function RefreshBtn({ children, title }: Props) {
     return (
-        <button className="flex rounded-full w-[67%] text-[#5C5F5D] bg-[#FAFB64] text-[20pt] font-bold mt-[19pt] pl-[23pt] pr-[10pt] py-[8pt] drop-shadow-xl sm:w-[30%]">
+        <button className="flex rounded-full w-[67%] text-[#5C5F5D] bg-[#FAFB64] text-[20px] font-bold mt-[19px] pl-[23px] pr-[10px] py-[8px] drop-shadow-xl sm:w-[30%]">
             {children}
             {title}
         </button>
