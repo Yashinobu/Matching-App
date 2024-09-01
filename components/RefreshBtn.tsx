053f3332948835abb3114d4283c52c0ea9e48f9c
Ellio <sereneheart888@gmail.com>
2024-09-01 16:58:32 +0300

    [U] update

--- components/RefreshBtn.tsx
+++ components/RefreshBtn.tsx
@@ -4,7 +4,7 @@ type Props = PropsWithChildren<{
 }>;
 export default function RefreshBtn({ children, title }: Props) {
     return (
-        <button className="flex rounded-full w-[67%] text-[#5C5F5D] bg-[#FAFB64] text-[20px] font-bold mt-[19px] pl-[23px] pr-[10px] py-[8px] drop-shadow-xl sm:w-[30%]">
+        <button className="flex rounded-full text-[#5C5F5D] bg-[#FAFB64] text-[20px] font-bold mt-[19px] pl-[23px] pr-[10px] py-[8px] drop-shadow-xl sm:w-[30%]">
             {children}
             {title}
         </button>
