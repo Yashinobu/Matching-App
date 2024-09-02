dc826de1e083cad8f06b519d922f56250eeb2cf1
Ellio <sereneheart888@gmail.com>
2024-09-02 13:26:49 +0300

    [U]

--- components/RefreshBtn.tsx
+++ components/RefreshBtn.tsx
@@ -4,7 +4,7 @@ type Props = PropsWithChildren<{
 }>;
 export default function RefreshBtn({ children, title }: Props) {
     return (
-        <button className="flex rounded-full text-[#5C5F5D] bg-[#FAFB64] text-[20px] font-bold mt-[19px] pl-[23px] pr-[10px] py-[8px] drop-shadow-xl 2xs:text-[18px] 2xs:mt-[17px] 2xs:pl-[21px] 2xs:pr-[9px] 2xs:py-[7px] xs:text-[16px] xs:mt-[15px] xs:pl-[18px] xs:pr-[8px] xs:py-[6px] sm:text-[20px] sm:mt-[19px] sm:pl-[23px] sm:pr-[10px] sm:py-[8px] md:text-[22px] md:mt-[21px] md:pl-[25px] md:pr-[11px] md:py-[9px]">
+        <button className="flex rounded-full text-[#5C5F5D] bg-[#FAFB64] text-[20px] font-bold mt-[19px] pl-[23px] pr-[10px] py-[8px] drop-shadow-xl 2xs:text-[18px] 2xs:mt-[17px] 2xs:pl-[21px] 2xs:pr-[9px] 2xs:py-[7px] xs:text-[16px] xs:mt-[10px] xs:pl-[18px] xs:pr-[8px] xs:py-[6px] sm:text-[20px] sm:mt-[19px] sm:pl-[23px] sm:pr-[10px] sm:py-[8px] md:text-[22px] md:mt-[21px] md:pl-[25px] md:pr-[11px] md:py-[9px]">
             {children}
             {title}
         </button>
--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 export default function SearchContent({ children }: PropsWithChildren) {
     return (
-        <div className="flex justify-center mt-[25px] w-[90%] h-[106px] gap-7 2xs:mt-[40px] xs:h-[85px] xs:mt-[20px] sm:h-[106px] sm:mt-[25px] md:h-[115px] md:mt-[28px]">{children}</div>
+        <div className="flex justify-center mt-[25px] w-[90%] h-[106px] gap-7 2xs:mt-[40px] xs:h-[85px] xs:mt-[10px] sm:h-[106px] sm:mt-[25px] md:h-[115px] md:mt-[28px]">{children}</div>
     );
 }
