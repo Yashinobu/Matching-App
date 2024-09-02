014f08eec72d4cc49b67a4f8ef9e6e0f65036fdf
Ellio <sereneheart888@gmail.com>
2024-09-02 10:22:12 +0300

    [U] responsive

--- components/Navbar/Bottom/index.tsx
+++ components/Navbar/Bottom/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function BottomNav({ children }: PropsWithChildren) {
     return (
-        <div className="flex bg-[#A5A5A5] w-full h-[98px] mt-[30.85px] rounded-t-2xl justify-between gap-10 py-[17px] pl-[45px] pr-[35px] sm:w-[40%]">
+        <div className="flex bg-[#A5A5A5] w-full h-[98px] mt-[30.85px] rounded-t-2xl justify-between gap-10 py-[17px] pl-[45px] pr-[35px] ">
             {children}
         </div>
     );
